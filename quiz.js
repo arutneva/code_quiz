3f2c77592bc24d76214126c4e7ecc9e414fce9ae
0magical <marilynventura@outlook.com>
2022-10-30 13:38:08 -0400

    initial commit with files, some pseudocode started

---
+++ quiz.js
@@ -0,0 +1,18 @@
+/*
+1. display start page
+    a. title
+    b. paragraph
+    c. start button
+
+2. timer starts when start button is clicked, 
+2a. initial page will be hidden, 
+2b. quiz page starts and shows question plus four answer buttons, 
+2c. when you click one show correct or wrong, deduct time based on answer by fifteen seconds when wrong
+
+3. once all questions are answered, timer stops and the time left becomes your score, you'll be presented with a score page and input initials + submit button
+
+4. show dashboard of all high scores
+
+
+
+*/
